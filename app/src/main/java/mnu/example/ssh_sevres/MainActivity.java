bec15eebf4c3d9c6306ba28e2b820ba721e49e99
Hans <zeunjiz86@gmail.com>
2022-01-15 00:32:02 +0900

    Initial commit

---
+++ app/src/main/java/mnu/example/ssh_sevres/MainActivity.java
@@ -0,0 +1,14 @@
+package mnu.example.ssh_sevres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/mnu/example/ssh_sevres/connection_sever.java
@@ -0,0 +1,11 @@
+package mnu.example.ssh_sevres;
+import java.io.InputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+import com.jcraft.jsch.SftpProgressMonitor;
+public class connection_sever {
+}
